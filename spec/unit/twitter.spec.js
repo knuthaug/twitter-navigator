826fca78bc664da5daa506e1f0d9ffc8948c5b87
Knut Haugen <knuthaug+github@gmail.com>
2010-08-11 08:14:46 +0200

    spec tests for twitter object

--- spec/unit/twitter.spec.js
+++ spec/unit/twitter.spec.js
@@ -1,25 +1,42 @@
 describe 'twitter'
 
   before_each
-    network = {};
-    neo = {};
-    stub(network, 'log').and_return( { } );
+    network = {}
+    neo = {}
+    stub(network, 'log').and_return( { } )
+    stub(neo, 'node_exists').and_return( { status: true, 
+                                           "data": {self:"http://localhost:9999/node/1"} })
+    stub(neo, 'get_node').and_return( {id: 123} )
     twitter = Twitter({"network": network, "neo": neo})
-    $ = {};
+    data = { }
   end
 
-  describe 'fetch_user'
-    it 'should call callback on fetch'
-      var called = 0;
-      stub(neo, 'node_exists').and_return( { status: true} );
-      stub(twitter, 'get_request').and_return( { } );
-      stub($, 'ajax').and_return( { } );
-      stub($, 'evalJSON').and_return( { } );
-      var user = twitter.lookup("foo", function(){
-                                called++;
-                      });
-      twitter.should.receive('get_request', 'once');
-      called.should.eql 1
+  describe 'user lookup'
+ 
+
+    it 'should call callback on fetch for valid user'
+      stub(neo, 'get_node').and_return( {id: 123} )
+
+      twitter.lookup("knuthaug", function(in_data){
+                   data = in_data;
+               });
+      data.id.should.eql 123
+    end
+
+    it 'should call callback with empty data on fetch for invalid user'
+      stub(neo, 'node_exists').and_return( { status: false} )
+      stub(neo, 'get_node').and_return( {} )
+      mock_request().and_return('{ }', 
+                                 'application/json', 
+                                 200, 
+                                 { Accept: 'application/json' })
+
+
+      twitter.lookup("fooooo", function(in_data){
+                   data = in_data;
+               });
+
+      data.should.not.have_property 'id'
     end
   end
 
