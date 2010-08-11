71543420106019ddfb26ae71a09b6500b6aa8006
Knut Haugen <knuthaug+github@gmail.com>
2010-08-11 11:38:21 +0200

    tests for ge_friends and get_followers

--- spec/unit/twitter.spec.js
+++ spec/unit/twitter.spec.js
@@ -4,15 +4,16 @@ describe 'twitter'
     network = {}
     neo = {}
     stub(network, 'log').and_return( { } )
-    stub(neo, 'node_exists').and_return( { status: true, 
+    stub(neo, 'node_exists').and_return( { status: true,
                                            "data": {self:"http://localhost:9999/node/1"} })
     stub(neo, 'get_node').and_return( {id: 123} )
     twitter = Twitter({"network": network, "neo": neo})
     data = { }
+    $ = { };
+    stub($, 'ajax').and_return(true);
   end
 
   describe 'user lookup'
- 
 
     it 'should call callback on fetch for valid user'
       stub(neo, 'get_node').and_return( {id: 123} )
@@ -26,9 +27,9 @@ describe 'twitter'
     it 'should call callback with empty data on fetch for invalid user'
       stub(neo, 'node_exists').and_return( { status: false} )
       stub(neo, 'get_node').and_return( {} )
-      mock_request().and_return('{ }', 
-                                 'application/json', 
-                                 200, 
+      mock_request().and_return('{ }',
+                                 'application/json',
+                                 200,
                                  { Accept: 'application/json' })
 
 
@@ -39,5 +40,37 @@ describe 'twitter'
       data.should.not.have_property 'id'
     end
   end
+
+  describe 'get friends'
+
+    it 'should call callback with data when fetching friends'
+       twitter.get_request = function(url, name, callback){
+          callback.call(twitter, [{ id: 1}, { id: 2}]);
+       };
+       twitter.get_friends("knuthaug", function(in_data){
+                            data = in_data;
+                          });
+
+      data.should.have_length 2
+    end
+
+  end
+
+  describe 'get followers'
+
+    it 'should call callback with data when fetching friends'
+       twitter.get_request = function(url, name, callback){
+          callback.call(twitter, [{ id: 1}, { id: 2}]);
+       };
+       twitter.get_followers("knuthaug", function(in_data){
+                            data = in_data;
+                          });
+
+      data.should.have_length 2
+    end
+
+  end
+
+
 
 end
